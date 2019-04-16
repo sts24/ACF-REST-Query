3ccc99a9d5b9e452bf7736ef19cb01cfa4b55759
Scott <stsmith@callutheran.edu>
2019-04-16 09:23:17 -0700

    added namespace to functions

--- acf-rest-query.php
+++ acf-rest-query.php
@@ -10,7 +10,7 @@
 
 
 
-function custom_request_param($args, $request){
+function sts_custom_request_param($args, $request){
 
 	foreach($_GET as $key=>$value){
 		if(strpos($key, 'acf_') !== false){
@@ -31,20 +31,20 @@ function custom_request_param($args, $request){
 	
 
 
-function custom_request($request){
+function sts_custom_request($request){
 
 	global $wp_query;
 	$post_types = get_post_types(array('_builtin'=>false,'public'=>true),'objects');
 
 	foreach($post_types as $pt){
 		
-		add_filter('rest_'.$pt->name.'_query', 'custom_request_param', 99,2);
+		add_filter('rest_'.$pt->name.'_query', 'sts_custom_request_param', 99,2);
 	}
-	add_filter('rest_post_query', 'custom_request_param', 99,2);
+	add_filter('rest_post_query', 'sts_custom_request_param', 99,2);
 
 
 }
 
-add_filter('rest_api_init', 'custom_request', 99, 2);
+add_filter('rest_api_init', 'sts_custom_request', 99, 2);
 
 ?>
