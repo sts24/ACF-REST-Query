d5cfb43faccfa304b45138a101cbd65ab61b0314
Scott Smith <scottsmith24@gmail.com>
2019-04-16 20:32:41 -0700

    GPL License added, function checks

--- acf-rest-query.php
+++ acf-rest-query.php
@@ -7,45 +7,50 @@
 	Description: Adds support for getting a REST API request with the parameter matching an Advanced Custom Field
 	Version: 1.0
 	Network: true
+	License: GPL-2.0+
+	License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
 
 
-function sts_custom_request_param($args, $request){
+if(!function_exists('sts_custom_request_param')){
+	
+	function sts_custom_request_param($args, $request){
 
-	foreach($_GET as $key=>$value){
-		if(strpos($key, 'acf_') !== false){
-			$args['meta_query'] = array(
-				array(
-					'key'     => str_replace('acf_','',$key),
-					'value'   => $_GET[$key],
-					'compare' => '=',
-				)
-			);
+		foreach($_GET as $key=>$value){
+			if(strpos($key, 'acf_') !== false){
+				$args['meta_query'] = array(
+					array(
+						'key'     => str_replace('acf_','',$key),
+						'value'   => $_GET[$key],
+						'compare' => '=',
+					)
+				);
+			}
 		}
+
+		return $args;
 	}
 
-	return $args;
 }
-
-
 	
 
+if(!function_exists('sts_custom_request')){
+	function sts_custom_request($request){
 
-function sts_custom_request($request){
+		global $wp_query;
+		$post_types = get_post_types(array('_builtin'=>false,'public'=>true),'objects');
 
-	global $wp_query;
-	$post_types = get_post_types(array('_builtin'=>false,'public'=>true),'objects');
+		foreach($post_types as $pt){
+			
+			add_filter('rest_'.$pt->name.'_query', 'sts_custom_request_param', 99,2);
+		}
+		add_filter('rest_post_query', 'sts_custom_request_param', 99,2);
 
-	foreach($post_types as $pt){
-		
-		add_filter('rest_'.$pt->name.'_query', 'sts_custom_request_param', 99,2);
 	}
-	add_filter('rest_post_query', 'sts_custom_request_param', 99,2);
 
+	add_filter('rest_api_init', 'sts_custom_request', 99, 2);
 
 }
-
-add_filter('rest_api_init', 'sts_custom_request', 99, 2);
 
 ?>
